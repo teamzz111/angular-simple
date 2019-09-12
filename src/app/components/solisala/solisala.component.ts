b83292fb72088ebd5ced61d169fe00750b74ffb8
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2019-09-11 21:27:19 -0500

    Class validator

--- src/app/components/solisala/solisala.component.ts
+++ src/app/components/solisala/solisala.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {Soli} from '../../models/soli';
+import {DynamicFormBuilder, DynamicFormGroup} from 'ngx-dynamic-form-builder';
 
 @Component({
   selector: 'app-solisala',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solisala.component.scss']
 })
 export class SolisalaComponent implements OnInit {
-
-  constructor() { }
+  public form: DynamicFormGroup<Soli>;
+  constructor(private fb: DynamicFormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group(Soli, {
+      date: new Date(), name: '', lab: '', asign: '', teacher: '', faculty: '', 
+      students: '', cantMachines: '', cantConsume: '', observation: '', useMachines: false,
+      useLab: false, guide: false, aux: false, useConsume: false
+    });
   }
 
 }
